b5687d43deee3a0792dd51c6e00092d25ca3439d
Ketan Sand <ketansand@Ketans-MacBook-Pro.local>
2026-06-03 18:12:49 -0400

    refactor(Repository-structure): Modified the pyproject from candiamazing to translist packages and also adjusted the readme file

---
+++ src/translist/__init__.py
@@ -0,0 +1,23 @@
+# from . import utils
+# from .core import BaseConverter, BrightnessConverter, DistanceConverter
+# from .test import test
+
+# try:
+#     from ._version import version as __version__  # noqa
+# except ImportError:
+#     __version__ = "0.0.0dev"
+
+# __author__ = "Connor Stone, Erik Osinga"
+
+# # List packages here to explicitly define the public API. Now candiamazing.<package> works.
+# __all__ = (
+#     "BaseConverter",
+#     "BrightnessConverter",
+#     "DistanceConverter",
+#     "utils",
+#     "test",
+#     "__version__",
+#     "__author__",
+# )
+
+#### Initialize all the main classes here ######
